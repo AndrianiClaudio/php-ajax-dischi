512f68777e1cf2b48227ea3c2e27ef6a0e13bd3b
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-02 17:31:56 +0100

    correzzione main.php - errore precedente copia

--- partials/template/main.php
+++ partials/template/main.php
@@ -3,12 +3,6 @@
     <?php
     // stampa delle cards
     include __DIR__ . './cards.php';
-    echo '<section class="card">';
-    echo '<img src='. $a['img_path'] . '>';
-    echo '<h2>' . $a['title'] . '</h2>';
-    echo '<span>' . $a['author'] . '</span>';
-    echo '<span>' . $a['year'] . '</span>';
-    echo '</section>';
     ?>
   </div>
 </main>
